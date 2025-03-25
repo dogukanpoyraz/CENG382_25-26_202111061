8a5fc14d660e7239363c95382af96e79b8f50fd7
Doğukan POYRAZ <dkpoyraz@hotmail.com>
2025-03-25 10:09:04 +0300

    .vscode configuration added.

--- LabProject/obj/Debug/net9.0/LabProject.AssemblyInfo.cs
+++ LabProject/obj/Debug/net9.0/LabProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f9ad48aae4ce759db64983ff95ea958e5e384c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b9375294296cd2990cb593799ae477f8222af10")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
